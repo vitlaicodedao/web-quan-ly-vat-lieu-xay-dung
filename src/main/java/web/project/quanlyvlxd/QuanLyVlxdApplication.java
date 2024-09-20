9b46de95a996c7e348005dc7304facd650fdf13c
tranviet0710 <trancongviet0710@gmail.com>
2024-09-20 14:31:56 +0700

    initialize project

---
+++ src/main/java/web/project/quanlyvlxd/QuanLyVlxdApplication.java
@@ -0,0 +1,13 @@
+package web.project.quanlyvlxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyVlxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyVlxdApplication.class, args);
+	}
+
+}
